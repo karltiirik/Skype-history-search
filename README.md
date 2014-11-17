6c7b9443f625422b9a8636bc658092fa1d95d951
Karl Tiirik <karltiirik@gmail.com>
2014-11-17 18:41:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Skype history searcher
+======
+A command line tool that accepts 2 arguments:
+* date (format 'YYYY-MM-DD')
+* Skype user display name
+and outputs all the messages from this user sent on given date
---
+++ search.py
@@ -0,0 +1,42 @@
+__author__ = 'Karl Tiirik'
+
+import sqlite3
+import argparse
+import datetime
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Search Skype messages by username and date')
+    parser.add_argument('date', type=str, help='a valid date in format DD.MM.YYYY')
+    parser.add_argument('name', type=str, help='Skype user display name')
+    arguments = parser.parse_args()
+    return arguments
+
+
+def validate_date(date_str):
+    try:
+        datetime.datetime.strptime(date_str, '%Y-%m-%d')
+        return True
+    except ValueError:
+        print('Incorrect data format, should be YYYY-MM-DD')
+        return False
+
+
+def get_messages(date, person):
+    db = sqlite3.connect('Database\\main.db')  # this needs os.path or smth smarter the future
+    c = db.cursor()
+    start_date = date + ' 00:00:00'
+    end_date = date + ' 24:59:99'
+    msgs = c.execute('SELECT m.body_xml FROM messages m WHERE DATETIME(m.timestamp, "unixepoch", "localtime") '
+                     'BETWEEN ? AND ? AND m.from_dispname = ?', (start_date, end_date, person,))
+    return msgs
+
+
+if __name__ == "__main__":  # Needs output formatting
+    args = parse_args()
+    print(args)
+    print(args.name)
+    if validate_date(args.date):
+        messages = get_messages(args.date, args.name)
+        for msg in messages:
+            print(msg)
